3cb5e79a6b8f693b40ae9fed1458fa07e40d3bd6
cfatrane <cfatrane@student.42.fr>
2017-03-11 19:53:18 +0100

    lem-in

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 11:36:27 by cfatrane          #+#    #+#             */
-/*   Updated: 2017/03/11 18:41:51 by cfatrane         ###   ########.fr       */
+/*   Updated: 2017/03/11 19:52:59 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int		lem_in(t_lem_in *env);
 
 int		ft_error_nbr(char *line);
 int		ft_error_rooms(char *name, char *x, char *y);
-int		ft_check_format_name(char *str);
 
 int		check_ants(t_lem_in *env);
 int		check_rooms(t_lem_in *env);
@@ -31,23 +30,19 @@ int		ft_end(t_lem_in *env, char *line);
 int		ft_links(t_lem_in *env, char *line);
 int		ft_comment(t_lem_in *env, char *line);
 
+int		ft_is_links(t_lem_in *env, char *line);
+
 int		hachage_lst(char *str);
 int		max_hash(t_lem_in *env);
 
-int		ft_is_links(t_lem_in *env, char *line);
 
 t_rooms	*ft_create_elem_rooms(char *name, int x, int y);
 void	ft_rooms_push_back(t_rooms **rooms, char *name, int x, int y);
 t_rooms	*ft_rooms_find(t_rooms *begin_list, int content_ref);
 void	ft_rooms_clear(t_rooms **begin_list);
-
 t_rooms	*ft_rooms_sort(t_rooms *lst);
+
 void	printlst(t_lem_in *env);
 void	ft_rooms_display(t_lem_in *env);
-
-int		parcours_hash(t_lem_in *env);
-
-int		error_rooms(t_lem_in *env);
-
 
 #endif
